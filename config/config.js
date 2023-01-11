c8f55c97a35c05324cfeb3eb5ed17849ef239cf4
Ateet Malviya <malviyaateet0009@gmail.com>
2023-01-11 16:29:19 +0530

    package added

---
+++ config/config.js
@@ -0,0 +1,26 @@
+/* Comman config */
+module.exports =
+{
+	"userTable"     	 : 'users',
+	"MYSQL_CREDENTIAL_DEVELOPMENT":{
+		"HOST":"localhost",
+		"USER":"root",
+		"PASSWORD":"",
+		"DATABASE":"adoro"
+	},
+	"MYSQL_CREDENTIAL_PRODUCTION":{
+		"HOST":"localhost",
+		"USER":"root",
+		"PASSWORD":"",
+		"DATABASE":"adoro"
+	},
+	"aws_bucket_name"	 : 'adoro/userimage',
+	"AWS_CREDENTIAL"     :{
+		"accessKeyId":"AKIA4EZQCBCZLYFKLJGU",
+		"secretAccessKey":"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"
+	},
+	"api_key" : "euqraM9b0125947a366fb9e5",
+	"templateID": "1707164905605342663",
+	"PEID": "1701164723775936270"
+}
+

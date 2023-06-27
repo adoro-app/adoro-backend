430f7c8a4d82cd10e1bb73ce435901114d096d8b
Ateet09 <malviyaateet0009@gmail.com>
2023-06-27 12:08:39 +0530

    addded

--- controllers/follow/UserFollowingController.js
+++ controllers/follow/UserFollowingController.js
@@ -224,12 +224,14 @@ exports.sendFollowRequest = async (req, res)=>{
                 let deleteRecord;
                 if (flag == 'feed'){
                      deleteRecord = await common.deleteRecords('follower', `user_id = ${id} AND follower_user_id = ${checkToken.id}` )
+                     let deleteNotiHistory = await common.deleteRecords('notification_history',`user_id = ${id} AND data_id = ${checkToken.id}`)
+            
                 }if(flag == 'delete'){
                      deleteRecord = await common.deleteRecords('follower', `user_id = ${checkToken.id} AND follower_user_id = ${id}` )
                      let deleteNotiHistory = await common.deleteRecords('notification_history',`user_id = ${id} AND data_id = ${checkToken.id}`)
                 }else{
                     deleteRecord = await common.deleteRecords('follower', `user_id = ${checkToken.id} AND follower_user_id = ${id}` )
-                    let deleteNotiHistory = await common.deleteRecords('notification_history',`user_id = ${id} AND data_id = ${checkToken.id}`)
+                    // let deleteNotiHistory = await common.deleteRecords('notification_history',`user_id = ${id} AND data_id = ${checkToken.id}`)
             
                 }
                 
